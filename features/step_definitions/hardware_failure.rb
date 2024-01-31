2482200d04acd60cf271de034fcebc430742b8fc
Sandro Knauß <hefee@debian.org>
2024-01-31 16:57:11 +0100

    send ready signal after the file exists.

--- features/step_definitions/hardware_failure.rb
+++ features/step_definitions/hardware_failure.rb
@@ -10,8 +10,8 @@ When /^Tails detects disk read failures$/ do
   squashfs_failed = '/var/lib/live/tails.squashfs_failed'
   $vm.execute('systemctl --now disable tails-detect-squashfs-errors')
   $vm.execute_successfully("touch #{squashfs_failed}")
-  RemoteShell::SignalReady.new($vm)
   try_for(60) { $vm.file_exist?(squashfs_failed) }
+  RemoteShell::SignalReady.new($vm)
 end
 
 Then /^I see a Disk Failure Message$/ do
