SchwabischesBauernbrot/tails

commit d723561eb66a8b8a2f1bcb77e3fbdfb63da4be2e

Make check more generic
As Ruff spotted, the call to subprocess.run hasn't check=True,
so it's up to us to deal with all failure modes manually,
so let's ensure we re-raise something for all non-zero exit codes.

config/chroot_local-includes/usr/lib/python3/dist-packages/additional_software.py
@@ -169,8 +169,7 @@ def notify(
         )
         if completed_process.stderr:
             logging.warning("%s", completed_process.stderr)
-        if completed_process.returncode == 1:
-            # sudo failed to execute the command
+        if completed_process.returncode != 0:
             raise OSError(completed_process.stderr)
     except OSError as e:
         logging.warning("Warning: unable to notify the user. %s" % e)

commit 6a20d89a92d4a3066f3b2c836482b10de59dc0f9

Fix displaying notification of Additional Software installation failure
a73ebb0f was not enough to fix the regression introduced by d723561e: we did not
support all the non-zero success exit codes, so notify() would return None, then
asp-install will try and fail to close that notification (by lack of a valid
ID), so finally the following notification about failure to install will (most
of the time?) fail to override the previous ("Installing") notification.

refs #20170

config/chroot_local-includes/usr/lib/python3/dist-packages/additional_software.py
@@ -152,7 +152,7 @@ def notify(
         urgent = ""
 
     try:
-        success_exit_codes = (0, 3, 5)
+        success_exit_codes = (0, 2, 3, 4, 5)
         completed_process = subprocess.run(  # noqa: PLW1510
             [  # noqa: S603
                 "/usr/local/lib/run-with-user-env",