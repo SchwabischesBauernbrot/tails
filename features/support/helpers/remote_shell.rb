5a6f32d44b876f679806e8757af649d79990938f
anonym <anonym@riseup.net>
2024-02-12 17:52:19 +0100

    Test suite, remote shell: stop with the server ACK when spawning commands
    Users (= intrigeri :)) have complained that it isn't expected. An we
    have reasons to think it causes:
    
        $vm.spawn('systemctl poweroff')
    
    to fail by the system under testing shutting down so fast the server
    wasn't able to send the ACK. It doesn't provide any value, so let's
    just drop it.
    
    Refs: tails/tails#20160

--- features/support/helpers/remote_shell.rb
+++ features/support/helpers/remote_shell.rb
@@ -37,6 +37,7 @@ module RemoteShell
     Object::Timeout.timeout(opts[:timeout], Timeout) do
       socket.puts(JSON.dump([id] + args))
       socket.flush
+      return if opts[:spawn]
       loop do
         # Calling socket.readline() and then just wait for the data to
         # arrive is prone to stalling for some reason. A timed read()
@@ -88,12 +89,14 @@ module RemoteShell
 
   class ShellCommand
     # If `:spawn` is false the server will block until it has finished
-    # executing `cmd`. If it's true the server won't block, and the
-    # response will always be [0, "", ""] (only used as an
-    # ACK). execute() will always block until a response is received,
-    # though. Spawning is useful when starting processes in the
+    # executing `cmd`, which in turn blocks the call of
+    # `execute()`. Otherwise, if `:spawn` is true, there is no
+    # blocking or even acknowledgement that the server received the
+    # remote call. Spawning is useful when starting processes in the
     # background (or running scripts that does the same) or any
-    # application we want to interact with.
+    # application we want to interact with. It is the caller's
+    # responsibility to some how (even implicitly) verify that the
+    # server executed the spawned command.
     def self.execute(vm, cmd, **opts)
       opts[:user] ||= 'root'
       opts[:spawn] = false unless opts.key?(:spawn)
