cf3957eca638e6a5f3b318b228afc956e1a4e0ca
Sandro Knauß <hefee@debian.org>
2024-03-05 19:01:14 +0100

    rename tails-detect-disk-errors -> tails-detect-disk-resize-errors.

--- config/chroot_local-hooks/52-update-rc.d
+++ config/chroot_local-hooks/52-update-rc.d
@@ -15,7 +15,7 @@ systemctl enable tails-allow-external-TailsData-access.service
 systemctl enable tails-autotest-broken-gnome-shell.service
 systemctl enable tails-autotest-remote-shell.service
 systemctl enable tails-create-netns.service
-systemctl enable tails-detect-disk-errors.service
+systemctl enable tails-detect-disk-resize-errors.service
 systemctl enable tails-detect-squashfs-errors.service
 systemctl enable tails-persistent-storage.service
 systemctl enable tails-remove-overlayfs-dirs.service
@@ -32,7 +32,7 @@ systemctl enable var-tmp.mount
 systemctl --global enable tails-add-GNOME-bookmarks.service
 systemctl --global enable tails-additional-software-install.service
 systemctl --global enable tails-configure-keyboard.service
-systemctl --global enable tails-report-disk-errors.service
+systemctl --global enable tails-report-disk-resize-errors.service
 systemctl --global enable tails-report-squashfs-errors.path
 systemctl --global enable tails-security-check.service
 systemctl --global enable tails-upgrade-frontend.service
