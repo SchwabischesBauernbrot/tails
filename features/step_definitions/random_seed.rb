d4e9ff139831c705c7850d410d20e017889a3d0e
segfault <segfault@riseup.net>
2024-03-20 17:28:46 +0100

    Test suite: Simplify random seed check
    Thanks to anonym for the great suggestions.

--- features/step_definitions/random_seed.rb
+++ features/step_definitions/random_seed.rb
@@ -2,65 +2,62 @@ def boot_log
   $vm.execute_successfully('cat /var/log/boot.log').stdout
 end
 
+def looks_random?(data)
+  chi_square = chi_squared(data)
+  debug_log("Randomness check chi-square = #{chi_square}")
+  # Based on https://gitlab.tails.boum.org/tails/blueprints/-/wikis/veracrypt/#detection
+  chi_square.between?(136, 426)
+end
+
+def chi_squared(data)
+  byte_counter = Array.new(256) { |i| data.bytes.count(i) }
+  e = data.length / 256.0
+  byte_counter.map { |n| (n - e)**2 }.sum / e
+end
+
+def bin_to_hex(data)
+  data.unpack1('H*')
+end
+
 Then /^there is (a|no) random seed on USB drive "([^"]+)"$/ do |randomness, name|
   should_be_random = (randomness == 'a')
-  path = $vm.storage.disk_path(name)
+
+  disk = {
+    path: $vm.storage.disk_path(name),
+    opts: {
+      format:   $vm.storage.disk_format(name),
+      readonly: true,
+    },
+  }
 
   # Store the old random seed for comparison
   if @random_seed
-    if @old_random_seed && File.exist?(@old_random_seed)
-      File.unlink(@old_random_seed)
-    end
     @old_random_seed = @random_seed
-    at_exit do
-      File.unlink(@old_random_seed) if File.exist?(@old_random_seed)
-    end
-  end
-
-  # Create an empty tempfile to store the random seed
-  f = Tempfile.create('random-seed', $config['TMPDIR'])
-  @random_seed = f.path
-  at_exit do
-    File.unlink(@random_seed) if File.exist?(@random_seed)
   end
 
   # Read the random seed from the USB drive
-  cmd = "guestfish --ro -a #{path} run : pread-device /dev/sda 512 17408 \
-         > #{@random_seed}"
-  debug_log("Executing command: #{cmd}")
-  p = IO.popen(cmd)
-  Process.wait(p.pid)
-  ret = $CHILD_STATUS
-  assert_equal(0, ret, 'Failed to read the random seed from the USB drive')
-  assert(File.exist?(@random_seed), 'Random seed file does not exist')
-  assert(File.size(@random_seed).positive?, 'Random seed file is empty')
+  offset = 512 * 34
+  @random_seed = $vm.storage.guestfs_disk_helper(disk) do |g, disk_handle|
+    g.pread_device(disk_handle, 512, offset)
+  end
+  assert_equal(512, @random_seed.length, 'Random seed is not 512 bytes long')
+  # Print the random seed for debugging, formatted as a hex string
+  debug_log("Random seed: #{bin_to_hex(@random_seed)}")
 
   # Check if the random seed is random
-  cmd = "#{GIT_DIR}/features/scripts/check-randomness.py < #{@random_seed}"
-  debug_log("Executing command: #{cmd}")
-  p = IO.popen(cmd)
-  out = p.readlines.join("\n")
-  Process.wait(p.pid)
-  ret = $CHILD_STATUS
-
-  # Print the output for debugging purposes
-  debug_log("Randomness check output: #{out}")
-
+  looks_random = looks_random?(@random_seed)
   if should_be_random
-    assert_equal(0, ret, "Randomness check failed: #{out}")
+    assert(looks_random, 'Randomness check failed')
   else
-    assert_not_equal(0, ret, "Randomness check succeded but should have failed: #{out}")
+    assert(!looks_random, 'Randomness check succeeded but should have failed')
   end
 end
 
 Then(/^the random seed is different from the previous one$/) do
   assert_not_nil(@old_random_seed, 'No previous random seed found')
   assert_not_nil(@random_seed, 'No random seed found')
-  old_seed = File.open(@old_random_seed, 'rb', &:read)
-  new_seed = File.open(@random_seed, 'rb', &:read)
-  assert(old_seed != new_seed,
-         "Random seed #{@random_seed} is the same as the previous one " \
-         "(#{@old_random_seed})")
+  assert(@old_random_seed != @random_seed,
+         'Random seed is the same as the previous one')
 end
 
 Then(/^the random seed was written multiple times on first boot$/) do
