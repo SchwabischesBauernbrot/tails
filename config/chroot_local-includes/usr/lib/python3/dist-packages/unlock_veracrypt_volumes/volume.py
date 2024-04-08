4ead9d67d2584c32b4486bb238312810cb4dd1df
segfault <segfault@riseup.net>
2024-04-08 13:15:15 +0200

    Fix Ruff INT002

--- config/chroot_local-includes/usr/lib/python3/dist-packages/unlock_veracrypt_volumes/volume.py
+++ config/chroot_local-includes/usr/lib/python3/dist-packages/unlock_veracrypt_volumes/volume.py
@@ -408,10 +408,8 @@ class Volume:
                 # they are placeholder and will be replaced. They need
                 # to be present in the translated string.
                 body = _(
-                    "Couldn't lock volume {volume_name}:\n{error_message}".format(
-                        volume_name=self.name, error_message=e.message
-                    )
-                )
+                    "Couldn't lock volume {volume_name}:\n{error_message}",
+                ).format(volume_name=self.name, error_message=e.message)
             self.manager.show_warning(_("Locking the volume failed"), body)
             return
 
