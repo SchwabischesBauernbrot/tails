1d69533c5704ecd937376be573fda64ac1ae4dbd
anonym <anonym@riseup.net>
2024-04-16 14:39:58 +0200

    Test suite: exempt a few more helpers from --interactive-debugging
    Until now I had mostly tested my very artificial scenarios in
    interactive_debugging.feature, but after running with our actual code
    it became clear that most of our helpers should be exempt since it is
    their callers' contexts that are interesting.

--- features/support/monkeypatches/interactive_debugging.rb
+++ features/support/monkeypatches/interactive_debugging.rb
@@ -49,13 +49,28 @@ class RubyVM::DebugInspector
       # return the binding in this method, find_our_caller_binding(),
       # raise() or Exception#initialize().
       false
+    when "#{GIT_DIR}/features/support/helpers/dogtail.rb"
+      # We are interested in the caller's context.
+      false
+    when "#{GIT_DIR}/features/support/helpers/firewall_helper.rb"
+      # We are interested in the caller's context.
+      false
     when "#{GIT_DIR}/features/support/helpers/misc_helpers.rb"
       # It's not in helpers like try_for(). When interactively
       # debugging we rarely are trying to debug those methods, but
       # rather the context they are called from, so we exempt them.
-      method != :pause && \
-        method != :try_for && \
-        method != :retry_action
+      method != :assert_vmcommand_success && \
+        method != :cmd_helper && \
+        method != :pause && \
+        method != :retry_action && \
+        method != :retry_tor && \
+        method != :try_for
+    when "#{GIT_DIR}/features/support/helpers/screen.rb"
+      # We are interested in the caller's context.
+      false
+    when "#{GIT_DIR}/features/support/helpers/vm_helper.rb"
+      # We are interested in the caller's context.
+      method != :execute_successfully
     when "#{GIT_DIR}/features/step_definitions/snapshots.rb"
       # It's not in the snapshot machinery, otherwise when an error
       # occurs during reach_checkpoint() or one of the generated
