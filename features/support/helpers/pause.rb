6b6cc652fa68a1679f1cab44cc980b257b16838f
anonym <anonym@riseup.net>
2024-04-19 15:17:31 +0200

    Test suite: log any of our methods we skip when injecting a pause() breakpoint

--- features/support/helpers/pause.rb
+++ features/support/helpers/pause.rb
@@ -1,62 +1,75 @@
 require 'binding_of_caller'
 require 'pry'
 
-# rubocop:disable Metrics/CyclomaticComplexity
-def our_caller_binding?(binding)
-  return false if binding.nil?
+def find_our_caller_binding(bindings, log_skipped: false)
+  skipped_methods = Array.new
+  our_caller_binding = bindings.find do |b|
+    next if b.nil?
 
-  source_location = binding.source_location&.first
-  # There are "weird" bindings that do not have source locations,
-  # probably some internal implementation detail of Ruby. We are
-  # only interested in our code, not code living in modules we
-  # import, or code we `include` (like Test::Unit::Assertions).
-  if source_location.nil? || !source_location&.start_with?(GIT_DIR)
-    return false
+    source_location = b.source_location&.first
+    # There are "weird" bindings that do not have source locations,
+    # probably some internal implementation detail of Ruby. We are
+    # only interested in our code, not code living in modules we
+    # import, or code we `include` (like Test::Unit::Assertions).
+    next if source_location.nil? || !source_location&.start_with?(GIT_DIR)
+
+    method = b.eval('__method__')
+
+    # We determine which bindings to return by matching against the
+    # calling method's name, which could lead to false positives if we
+    # ever define methods with the same name in other contexts
+    # (e.g. in some class). Therefore we also guard by which file they
+    # are defined in.
+    # When interactively debugging we rarely are trying to debug
+    # various helper methods but rather the context they are called
+    # from, so we exempt them below.
+    our = case source_location
+          when __FILE__
+            # Everything in this file is ignored, otherwise we would always
+            # return the binding in this method, find_our_caller_binding() or
+            # pause().
+            false
+          when "#{GIT_DIR}/features/step_definitions/snapshots.rb"
+            # We ignore the snapshot machinery, otherwise when an error occurs
+            # during reach_checkpoint() or one of the generated snapshot steps
+            # we end up with those not very interesting contexts.
+            false
+          when "#{GIT_DIR}/features/support/helpers/dogtail.rb"
+            false
+          when "#{GIT_DIR}/features/support/helpers/firewall_helper.rb"
+            false
+          when "#{GIT_DIR}/features/support/helpers/misc_helpers.rb"
+            method != :assert_vmcommand_success && \
+            method != :cmd_helper && \
+            method != :retry_action && \
+            method != :retry_tor && \
+            method != :try_for
+          when "#{GIT_DIR}/features/support/helpers/screen.rb"
+            false
+          when "#{GIT_DIR}/features/support/helpers/vm_helper.rb"
+            method != :execute_successfully
+          else
+            true
+          end
+    skipped_methods << method unless our
+    our
   end
-
-  method = binding.eval('__method__')
-
-  # We determine which bindings to return by matching against the
-  # calling method's name, which could lead to false positives if we
-  # ever define methods with the same name in other contexts
-  # (e.g. in some class). Therefore we also guard by which file they
-  # are defined in.
-  # When interactively debugging we rarely are trying to debug
-  # various helper methods but rather the context they are called
-  # from, so we exempt them below.
-  case source_location
-  when __FILE__
-    # Everything in this file is ignored, otherwise we would always
-    # return the binding in this method, find_our_caller_binding() or
-    # pause().
-    false
-  when "#{GIT_DIR}/features/step_definitions/snapshots.rb"
-    # We ignore the snapshot machinery, otherwise when an error occurs
-    # during reach_checkpoint() or one of the generated snapshot steps
-    # we end up with those not very interesting contexts.
-    false
-  when "#{GIT_DIR}/features/support/helpers/dogtail.rb"
-    false
-  when "#{GIT_DIR}/features/support/helpers/firewall_helper.rb"
-    false
-  when "#{GIT_DIR}/features/support/helpers/misc_helpers.rb"
-    method != :assert_vmcommand_success && \
-      method != :cmd_helper && \
-      method != :retry_action && \
-      method != :retry_tor && \
-      method != :try_for
-  when "#{GIT_DIR}/features/support/helpers/screen.rb"
-    false
-  when "#{GIT_DIR}/features/support/helpers/vm_helper.rb"
-    method != :execute_successfully
-  else
-    true
+  if log_skipped && skipped_methods.size.positive?
+    $stderr.puts
+    $stderr.puts '  Skipped context of our helpers:'
+    # Array#uniq preserves order, so it works like an ordered set,
+    # which Ruby lacks. We want that functionality in order to not
+    # repeat which methods was skipped since there can be several
+    # caller bindings in the same method (e.g. loops create a new
+    # one).
+    skipped_methods.uniq.each do |method|
+      $stderr.puts("  - #{method}()")
+    end
+    $stderr.puts
+    $stderr.puts 'If you are currently debugging any of those methods you might want to'
+    $stderr.puts "temporarily add an exemption for them in #{__method__}()"
   end
-end
-# rubocop:enable Metrics/CyclomaticComplexity
-
-def find_our_caller_binding(bindings)
-  bindings.find { |b| our_caller_binding?(b) }
+  our_caller_binding
 end
 
 def pause(message = 'Paused', exception: nil, quiet: false)
@@ -79,14 +92,20 @@ def pause(message = 'Paused', exception: nil, quiet: false)
         # binding's stack of caller bindings, provided by the
         # binding_of_caller module.
         caller_bindings = binding.callers
+        # When manually adding a pause() breakpoint we log which of
+        # our methods we skip in find_our_caller_binding(). This is a
+        # nice reminder of which methods we skip in case it is one of
+        # them we are debugging and added the breakpoint into.
+        log_skipped = true
       else
         # pause() was called for an exception caught by cucumber so
         # our caller is in the binding stack in the exception provided
         # by the bindex module.
         assert(config_bool('INTERACTIVE_DEBUGGING'))
         caller_bindings = exception.bindings
+        log_skipped = false
       end
-      our_caller_binding = find_our_caller_binding(caller_bindings)
+      our_caller_binding = find_our_caller_binding(caller_bindings, log_skipped:)
       if our_caller_binding.nil?
         warn "Warning: could not restore the failure's context"
         $stderr.puts
