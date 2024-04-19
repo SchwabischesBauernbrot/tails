9fcecb8a3c6f89c0e94ac142c5d75eedb8e04084
anonym <anonym@riseup.net>
2024-04-19 17:52:24 +0200

    Test suite: add commands to Pry to navigate and show the stack
    The commands are: down, up, stack

--- features/support/helpers/pause.rb
+++ features/support/helpers/pause.rb
@@ -1,8 +1,26 @@
 require 'binding_of_caller'
 require 'pry'
 
+def binding_display(b)
+  method = b.eval('__method__')
+  instance = b.eval('self')
+  if instance.instance_of?(Object)
+    # The top-level Object instance is a mess when displayed,
+    # "#<Object+RSpec::Matchers+Cucumber::RbSupport::RbWorld+...>"
+    # or similar, so we strip all that.
+    instance = '#<Object>'
+  end
+  if method.nil? || method.empty?
+    what = '<top-level>'
+  else
+    what = "#{method}()"
+  end
+  where = "#{b.source_location.join(':')}"
+  "#{Pry::Helpers::Text.bold(what)} (#{instance}) at #{where}"
+end
+
 def find_our_caller_binding(bindings, log_skipped: false)
-  skipped_methods = Array.new
+  skipped_bindings = Array.new
   our_caller_binding = bindings.find do |b|
     next if b.nil?
 
@@ -51,23 +69,20 @@ def find_our_caller_binding(bindings, log_skipped: false)
           else
             true
           end
-    skipped_methods << method unless our
+    skipped_bindings << binding_display(b) unless our
     our
   end
-  if log_skipped && skipped_methods.size.positive?
+  if log_skipped && skipped_bindings.size.positive?
     $stderr.puts
-    $stderr.puts '  Skipped context of our helpers:'
-    # Array#uniq preserves order, so it works like an ordered set,
-    # which Ruby lacks. We want that functionality in order to not
-    # repeat which methods was skipped since there can be several
-    # caller bindings in the same method (e.g. loops create a new
-    # one).
-    skipped_methods.uniq.each do |method|
-      $stderr.puts("  - #{method}()")
+    $stderr.puts Pry::Helpers::Text.bold(
+                   '  Helpers were skipped in the stack (above the arrow):'
+                 )
+    $stderr.puts
+    skipped_bindings.each do |binding_display|
+      $stderr.puts("     #{binding_display}")
     end
-    $stderr.puts
-    $stderr.puts 'If you are currently debugging any of those methods you might want to'
-    $stderr.puts "temporarily add an exemption for them in #{__method__}()"
+    $stderr.puts " =>  #{binding_display(our_caller_binding)}"
+    $stderr.puts '     [...]'
   end
   our_caller_binding
 end
@@ -105,16 +120,71 @@ def pause(message = 'Paused', exception: nil, quiet: false)
         caller_bindings = exception.bindings
         log_skipped = false
       end
+      $caller_bindings = caller_bindings
       our_caller_binding = find_our_caller_binding(caller_bindings, log_skipped:)
+      $caller_bindings_index = $caller_bindings.find_index(our_caller_binding)
       if our_caller_binding.nil?
         warn "Warning: could not restore the failure's context"
         $stderr.puts
         quiet = true
         our_caller_binding = binding
       end
+      $stderr.puts
+      $stderr.puts <<-MESSAGE
+  Use the 'stack' command to see where you are in the stack, and the 'down'
+  and 'up' commands to navigate the stack.
+      MESSAGE
       our_caller_binding.pry(quiet:)
     end
   end
 end
 
 alias breakpoint pause
+
+def pry_navigate_caller_stack(offset)
+  new_index = $caller_bindings_index + offset
+  if new_index < 0
+    raise(Pry::CommandError, 'Top of stack reached!')
+  elsif new_index >= $caller_bindings.size
+    raise(Pry::CommandError, 'Bottom of stack reached!')
+  end
+  $caller_bindings_index = new_index
+  new_binding = $caller_bindings[$caller_bindings_index]
+  if pry_instance.binding_stack.empty?
+    pry_instance.binding_stack.replace [new_binding]
+  else
+    pry_instance.binding_stack[-1] = new_binding
+  end
+  pry_instance.run_command("whereami")
+end
+
+StackCommands = Pry::CommandSet.new do
+  create_command 'up', "Move up in the call stack" do
+    def process
+      pry_navigate_caller_stack(-1)
+    end
+  end
+
+  create_command "down", "Move down in the call stack" do
+    def process
+      pry_navigate_caller_stack(1)
+    end
+  end
+
+  create_command "stack", "Print the stack" do
+    def process
+      current = $caller_bindings[$caller_bindings_index]
+      stack = $caller_bindings.map do |b|
+        indicator = b == current ? '=>' : '  '
+        "#{indicator} #{binding_display(b)}"
+      end
+      message = bold('Stack: <method> (<instance>) at <source location>')
+      message += "\n"
+      message += "\n  "
+      message += stack.join("\n  ")
+      stagger_output(message)
+    end
+  end
+end
+
+Pry.config.commands.import(StackCommands)
