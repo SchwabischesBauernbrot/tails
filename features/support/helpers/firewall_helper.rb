b4e99e0859ee6858d29a7e63c54f62a9fb90097c
anonym <anonym@riseup.net>
2024-09-02 10:51:29 +0200

    Test suite: refactor and simplify
    RuboCop likes this!
    
    Also there's no reason why we need to handle each packet type on
    case-by-case since PacketFu::Packet.parse() will parse anything and
    return the most specific class.

--- features/support/helpers/firewall_helper.rb
+++ features/support/helpers/firewall_helper.rb
@@ -1,121 +1,89 @@
 require 'packetfu'
 require 'net/dns'
 
-def looks_like_dhcp_packet?(eth_packet, protocol, sport, dport, ip_packet)
-  protocol == 'udp' && sport == 68 && dport == 67 &&
-    eth_packet.eth_daddr == 'ff:ff:ff:ff:ff:ff' &&
-    ip_packet && ip_packet.ip_daddr == '255.255.255.255'
+def looks_like_dhcp_packet?(packet)
+  packet.instance_of?(PacketFu::UDPPacket) &&
+    packet.udp_sport == 68 &&
+    packet.udp_dport == 67 &&
+    packet.eth_daddr == 'ff:ff:ff:ff:ff:ff' &&
+    packet.ip_daddr == '255.255.255.255'
 end
 
-def rarp_packet?(packet)
+def rarp_packet?(pcap_packet)
   # Details: https://www.netometer.com/qa/rarp.html#A13
-  packet.force_encoding('UTF-8').start_with?(
+  pcap_packet.force_encoding('UTF-8').start_with?(
     "\xFF\xFF\xFF\xFF\xFF\xFFRT\x00\xAC\xDD\xEE\x805\x00\x01\b\x00\x06"
-  ) && (packet[19] == "\x03" || packet[19] == "\x04")
+  ) && (pcap_packet[19] == "\x03" || pcap_packet[19] == "\x04")
+end
+
+def connection_info(packet)
+  connection = {
+    mac_saddr: packet.eth_saddr,
+    mac_daddr: packet.eth_daddr,
+    protocol:  (packet.proto - ['Eth']).join('::'),
+    saddr:     nil,
+    daddr:     nil,
+  }
+
+  begin
+    connection[:saddr] = packet.ip_saddr
+    connection[:daddr] = packet.ip_daddr
+  rescue NameError
+    begin
+      connection[:saddr] = packet.ipv6_saddr
+      connection[:daddr] = packet.ipv6_daddr
+    rescue NameError
+      puts "We were hit by #11508. PacketFu bug? Packet info:\n#{packet}"
+    end
+  end
+
+  case packet
+  when PacketFu::TCPPacket
+    connection[:sport] = packet.tcp_sport
+    connection[:dport] = packet.tcp_dport
+  when PacketFu::UDPPacket
+    connection[:sport] = packet.udp_sport
+    connection[:dport] = packet.udp_dport
+    if packet.udp_dport == 53
+      begin
+        dns_packet = Net::DNS::Packet.parse(packet.payload)
+      rescue ArgumentError
+        dns_packet = nil
+      end
+      unless dns_packet.nil? || dns_packet.question.empty?
+        connection[:dns_question] = dns_packet.question.map(&:qName)
+      end
+    end
+  end
+
+  connection
 end
 
 # Returns the unique edges (based on protocol, source/destination
 # address/port) in the graph of all network flows.
-# XXX: giving up on a few worst offenders for now
-# rubocop:disable Metrics/AbcSize
-# rubocop:disable Metrics/BlockLength
-# rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/MethodLength
-# rubocop:disable Metrics/PerceivedComplexity
 def pcap_connections_helper(pcap_file,
                             ignore_arp: true,
                             ignore_dhcp: true,
                             ignore_sources: [$vm.vmnet.bridge_mac])
   connections = []
-  packets = PacketFu::PcapFile.new.file_to_array(filename: pcap_file)
-  packets.each do |p|
-    if PacketFu::EthPacket.can_parse?(p)
-      eth_packet = PacketFu::EthPacket.parse(p)
-    elsif rarp_packet?(p)
-      # packetfu cannot parse RARP, see #16825.
-      next
-    else
+  PacketFu::PcapFile.new.file_to_array(filename: pcap_file).map do |pcap_packet|
+    # PacketFu cannot parse RARP, see #16825. We consider RARP safe.
+    next if rarp_packet?(pcap_packet)
+
+    packet = PacketFu::Packet.parse(pcap_packet)
+    if packet.instance_of?(PacketFu::InvalidPacket)
       raise FirewallAssertionFailedError,
-            'Found something that is not an ethernet packet'
-    end
-    sport = nil
-    dport = nil
-    dns_question = []
-    if PacketFu::IPv6Packet.can_parse?(p)
-      ip_packet = PacketFu::IPv6Packet.parse(p)
-      protocol = 'ipv6'
-    elsif PacketFu::TCPPacket.can_parse?(p)
-      ip_packet = PacketFu::TCPPacket.parse(p)
-      protocol = 'tcp'
-      sport = ip_packet.tcp_sport
-      dport = ip_packet.tcp_dport
-    elsif PacketFu::UDPPacket.can_parse?(p)
-      ip_packet = PacketFu::UDPPacket.parse(p)
-      protocol = 'udp'
-      sport = ip_packet.udp_sport
-      dport = ip_packet.udp_dport
-    elsif PacketFu::ICMPPacket.can_parse?(p)
-      ip_packet = PacketFu::ICMPPacket.parse(p)
-      protocol = 'icmp'
-    elsif PacketFu::IPPacket.can_parse?(p)
-      ip_packet = PacketFu::IPPacket.parse(p)
-      protocol = 'ip'
-    elsif PacketFu::ARPPacket.can_parse?(p)
-      ip_packet = PacketFu::ARPPacket.parse(p)
-      protocol = 'arp'
-    else
-      raise FirewallAssertionFailedError,
-            'Found something that cannot be parsed'
+            'Found something PacketFu cannot parse'
     end
 
-    next if ignore_dhcp &&
-            looks_like_dhcp_packet?(eth_packet, protocol,
-                                    sport, dport, ip_packet)
-    next if ignore_arp && protocol == 'arp'
-    next if ignore_sources.include?(eth_packet.eth_saddr)
+    next if ignore_arp && packet.instance_of?(PacketFu::ARPPacket)
+    next if ignore_dhcp && looks_like_dhcp_packet?(packet)
+    next if ignore_sources.include?(packet.eth_saddr)
 
-    if protocol == 'udp' && dport == 53
-      begin
-        dns_packet = Net::DNS::Packet.parse(PacketFu::Packet.parse(p).payload)
-      rescue ArgumentError
-        dns_packet = nil
-      end
-      unless dns_packet.nil? || dns_packet.question.empty?
-        dns_question += dns_packet.question.map(&:qName)
-      end
-    end
-
-    packet_info = {
-      mac_saddr:    eth_packet.eth_saddr,
-      mac_daddr:    eth_packet.eth_daddr,
-      protocol:,
-      sport:,
-      dport:,
-      dns_question:,
-    }
-
-    begin
-      packet_info[:saddr] = ip_packet.ip_saddr
-      packet_info[:daddr] = ip_packet.ip_daddr
-    rescue NameError
-      begin
-        packet_info[:saddr] = ip_packet.ipv6_saddr
-        packet_info[:daddr] = ip_packet.ipv6_daddr
-      rescue NameError
-        puts "We were hit by #11508. PacketFu bug? Packet info: #{ip_packet}"
-        packet_info[:saddr] = nil
-        packet_info[:daddr] = nil
-      end
-    end
-    connections << packet_info
+    connections << connection_info(packet)
   end
   connections.uniq.map { |p| OpenStruct.new(p) }
 end
-# rubocop:enable Metrics/AbcSize
-# rubocop:enable Metrics/BlockLength
-# rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/MethodLength
-# rubocop:enable Metrics/PerceivedComplexity
 
 class FirewallAssertionFailedError < Test::Unit::AssertionFailedError
 end
@@ -151,13 +119,16 @@ def assert_no_leaks(pcap_file, allowed_hosts, allowed_dns_queries, **opts)
   dns_opts = opts.clone
   dns_opts[:message] = 'Unexpected DNS queries were made'
   assert_all_connections(pcap_file, **dns_opts) do |c|
-    c.dns_question.all? { |q| allowed_dns_queries.include?(q) }
+    !defined?(c.dns_question) ||
+      c.dns_question.all? { |q| allowed_dns_queries.include?(q) }
   end
 end
 
 def debug_useless_dns_exceptions(pcap_file, allowed_dns_queries)
   queries_made = Set.new
-  pcap_connections_helper(pcap_file).each do |c|
+  pcap_connections_helper(pcap_file) do |c|
+    next unless defined?(c.dns_question)
+
     queries_made += c.dns_question
   end
   queries_allowed = Set.new(allowed_dns_queries)
