2e2121d549626fbbb6fdf1aaac00007a4df29fbb
intrigeri <intrigeri@boum.org>
2024-10-21 10:11:39 +0000

    initramfs boot scripts: add missing support for the prereqs argument
    Same as d316a3c1fee594ddbe77c8ad21126828a2df788e: boot scripts are run *while
    building the initrd* with this argument.
    
    refs #20623

--- config/chroot_local-includes/usr/share/initramfs-tools/scripts/lib/lib.sh
+++ config/chroot_local-includes/usr/share/initramfs-tools/scripts/lib/lib.sh
@@ -1,5 +1,16 @@
 # shellcheck shell=ash
 
+PREREQS=""
+
+prereqs() { echo "$PREREQS"; }
+
+case "${1:-}" in
+prereqs)
+    prereqs
+    exit 0
+    ;;
+esac
+
 log(){
   echo "$(date "+%H:%M:%S.%3N") $*"
 }
